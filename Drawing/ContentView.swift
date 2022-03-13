8bfbf51ba91fb81a60ed31fb2ad41a2d86727dc7
Takasur Azeem <takasurazeem@gmail.com>
2022-03-14 03:09:29 +0500

    Initial Commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Takasur Azeem on 14/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
